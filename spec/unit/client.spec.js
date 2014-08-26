db5675a64489cd6db515853da16b95d36ba8f52d
Jeff Parrish <jparrish@digium.com>
2014-08-26 14:08:27 -0700

    join and leave events and tests

--- spec/unit/client.spec.js
+++ spec/unit/client.spec.js
@@ -8,7 +8,7 @@ var Client = respoke.Client;
 describe('respoke.Client', function () {
     this.timeout(10000);
 
-    describe('brokered auth', function () {
+    describe('brokered auth connection', function () {
         var admin;
         var appId;
         var roleId;
@@ -43,7 +43,7 @@ describe('respoke.Client', function () {
                             appId: appId,
                             roleId: roleId,
                             endpointId: endpointId,
-                            ttl: 20
+                            ttl: 200
                         }, function (err, authInfo) {
                             if (err) {
                                 return done(err);
@@ -107,35 +107,7 @@ describe('respoke.Client', function () {
         });
     });
 
-    describe('subscribers', function () {
-        
-        var client = null;
-
-        beforeEach(function (done) {
-            client = new Client({
-                appId: helpers.appId,
-                endpointId: "bot-" + uuid.v4(),
-                developmentMode: true,
-                baseURL: helpers.baseURL
-            });
-            client.on('connect', function (data) {
-                done();
-            });
-            client.on('error', function (err) {
-                done(err);
-            });
-        });
-
-        it('gets all subscribers', function (done) {
-            client.getAllSubscribers(function (err, subscribers) {
-                should.not.exist(err);
-                subscribers.should.be.an.Array;
-                done();
-            });
-        });
-    });
-
-    describe('messages', function () {
+    describe('messages and groups', function () {
         var endpointId1 = "client1-" + uuid.v4();
         var endpointId2 = "client2-" + uuid.v4();
         var client1 = null;
@@ -143,6 +115,8 @@ describe('respoke.Client', function () {
         var createdClients = 0;
 
         beforeEach(function (done) {
+            createdClients = 0;
+
             client1 = new Client({
                 appId: helpers.appId,
                 endpointId: endpointId1,
@@ -186,9 +160,20 @@ describe('respoke.Client', function () {
                     done();
                 }
             };
-            client1.close(handler);
-            client2.close(handler);
-            createdClients = 0;
+            if (client1.isConnected()) {
+                client1.close(handler);
+            } 
+            else {
+                createdClients++;
+                handler();
+            }
+            if (client2.isConnected()) {
+                client2.close(handler);
+            }
+            else {
+                createdClients++;
+                handler();
+            }
         });
 
         // client 1 sending a message to client 2
@@ -207,6 +192,83 @@ describe('respoke.Client', function () {
                     return done(err);
                 }
             });
+        });
+
+        it('lists groups members and observes presence', function (done) {
+            var groupId = 'somegroup-' + uuid.v4();
+            var totalJoined = 0;
+            var msgText = "Hey - " + uuid.v4();
+
+            var errHandler = function (err) {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                totalJoined++;
+                if (totalJoined === 2) {
+                    doTest();
+                }
+            };
+
+            client1.join({ groupId: groupId }, errHandler);
+            client2.join({ groupId: groupId }, errHandler);
+
+            function doTest() {
+
+                // Make sure both members are in the group
+                client1.getGroupMembers({ groupId: groupId }, function (err, members) {
+                    if (err) {
+                        return done(err);
+                    }
+                    members.should.be.an.Array;
+                    var hasClient1 = false;
+                    var hasClient2 = false;
+                    for (var i = 0; i < members.length; i++) {
+                        var memb = members[i];
+                        memb.endpointId.should.be.a.String;
+                        memb.connectionId.should.be.a.String;
+                        if (memb.endpointId === endpointId1) {
+                            hasClient1 = true;
+                        }
+                        if (memb.endpointId === endpointId2) {
+                            hasClient2 = true;
+                        }
+                    }
+                    hasClient1.should.be.ok;
+                    hasClient2.should.be.ok;
+
+                    // register presence
+                    var endpoints = members.map(function (memb) {
+                        return memb.endpointId;
+                    });
+                    var testStatus = 'At lunch';
+                    client1.registerPresence(endpoints, function (err) {
+                        if (err) {
+                            done(err);
+                        }
+                    });
+
+                    // the test
+                    client1.on('presence', function (data) {
+                        data.status.should.equal(testStatus);
+                        data.header.type.should.equal('presence');
+                        data.header.from.should.equal(endpointId2);
+                        data.header.fromConnection.should.be.a.String;
+                        data.type.should.equal('available');
+                        done();
+                    });
+
+                    // ensure there was time to subscribe
+                    setTimeout(function () {
+                        client2.setPresence({ status: testStatus }, function (err) {
+                            if (err) {
+                                done(err);
+                            }
+                        });
+                    }, 1000);
+
+                });
+            }
         });
 
         it('sends and receives group messages', function (done) {
@@ -229,13 +291,14 @@ describe('respoke.Client', function () {
             client2.join({ groupId: groupId }, errHandler);
 
             function doTest() {
-                client2.on('message', function (data) {
-                    data.header.type.should.equal('pubsub');
+                client2.on('pubsub', function (data) {
                     data.header.from.should.equal(endpointId1);
                     data.header.groupId.should.equal(groupId);
+                    data.header.type.should.equal('pubsub');
+                    data.message.should.equal(msgText);
                     done();
                 });
-                client1.sendMessage({
+                client1.sendGroupMessage({
                     groupId: groupId,
                     message: msgText
                 }, function (err) {
@@ -245,6 +308,69 @@ describe('respoke.Client', function () {
                 });
             }
         });
+
+        it('gets join and leave events', function (done) {
+            var groupId = 'somegroup-' + uuid.v4();
+            var totalJoined = 0;
+            var msgText = "Hey - " + uuid.v4();
+            var gotJoin = false;
+            var alreadyDoned = false;
+
+            client1.join({ groupId: groupId }, function (err) {
+                if (err) {
+                    return done(err);
+                }
+                client1.on('join', function (data) {
+                    if (data.endpointId === endpointId1) {
+                        // ignore self;
+                        return;
+                    }
+                    data.header.type.should.equal('join');
+                    data.header.groupId.should.equal(groupId);
+                    data.endpointId.should.equal(endpointId2);
+                    data.connectionId.should.be.a.String;
+                    gotJoin = true;
+                });
+
+                setTimeout(function () {
+
+                    client2.join({ groupId: groupId }, function (err) {
+                        if (err) {
+                            return done(err);
+                        }
+                        setTimeout(function () {
+                            client2.leave({ groupId: groupId }, function (err) {
+                                if (err) {
+                                    return done(err);
+                                }
+                            });
+                        }, 500);
+                    });
+
+                }, 500);
+            });
+
+            client1.on('leave', function (data) {
+                data.header.type.should.equal('leave');
+                data.header.groupId.should.equal(groupId);
+                data.endpointId.should.equal(endpointId2);
+                data.connectionId.should.be.a.String;
+
+                if (!gotJoin) {
+                    return done(new Error("Did not get join event"));
+                }
+                // some of the connections are not getting cleaned up properly.
+                // this leave is getting called more than once with the same endpointId but
+                // different connectionIds
+                if (!alreadyDoned) {
+                    alreadyDoned = true;
+                    done();
+                }
+            });
+
+        });
+
+
     });
 
 });
