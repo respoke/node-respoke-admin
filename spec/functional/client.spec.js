7141361bd3f85027749c1a225f737c7c785ce1b0
Matthew Turney <mturney@digium.com>
2014-10-09 14:07:49 -0500

    Split tests into functional and units

--- spec/functional/client.spec.js
+++ spec/functional/client.spec.js
@@ -1,53 +1,13 @@
 'use strict';
-var Respoke = require('../index');
+var Respoke = require('../../index');
 var should = require('should');
-var helpers = require('./helpers');
+var helpers = require('../helpers');
 var uuid = require('uuid');
 
 var respoke;
 
-describe('Respoke', function () {
+describe('Respoke functional', function () {
     this.timeout(10000);
-
-    describe('Base methods and properties', function () {
-        beforeEach(function () {
-            respoke = new Respoke({
-                baseURL: helpers.baseURL
-            });
-        });
-
-        it('has request', function () {
-            respoke.request.should.be.a.Function;
-        });
-
-        it('has wsCall', function () {
-            respoke.wsCall.should.be.a.Function;
-        });
-
-        it('has noop', function () {
-            respoke.noop.should.be.a.Function;
-        });
-
-        it('is an EventEmitter', function () {
-            respoke.on.should.be.a.Function;
-            respoke.emit.should.be.a.Function;
-        });
-
-        it('has expected properties', function () {
-            respoke.tokens.should.be.an.Object;
-            (respoke.connectionId === null).should.be.ok;
-            (respoke.socket === null).should.be.ok;
-            respoke.baseURL.should.be.a.String;
-            respoke.baseURL.should.not.be.empty;
-
-            respoke.auth.should.be.an.Object;
-            respoke.groups.should.be.an.Object;
-            respoke.messages.should.be.an.Object;
-            respoke.roles.should.be.an.Object;
-            respoke.apps.should.be.an.Object;
-            respoke.presence.should.be.an.Object;
-        });
-    });
 
     describe('Authentication', function () {
 
